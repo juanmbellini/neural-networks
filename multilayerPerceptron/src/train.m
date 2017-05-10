a569f6a335fa24f3c1b1b4c497dccf63d858b50d
fbartolome <fbartolome18@gmail.com>
2017-05-10 16:34:20 -0300

    Fix weight calculation.
    Fix weight calculation.
    Nets folder in gitignore.

--- multilayerPerceptron/src/train.m
+++ multilayerPerceptron/src/train.m
@@ -15,24 +15,24 @@
 
     n = 0.05;
     beta = 1;
-    iterations = 99999;
+    iterations = 2000;
 
     activationFunctions = activationFunctions();
     normalizers = normalizers();
     denormalizers = denormalizers();
 
-    g = activationFunctions.tangHyp;
-    gDeriv = activationFunctions.tangHypDeriv;
+    g = activationFunctions.expo;
+    gDeriv = activationFunctions.expoDeriv;
 
     hiddenLayerSizes = [6 4 4 4];
-    error = 0.1;
+    error = 0;
 
-    psiNormalizer = normalizers.entryTangHypNormalizerFromMinus4_4;
-    sNormalizer = normalizers.tangHypNormalizerFromMinus10_10;
-    denormalizer = denormalizers.tangHypDenormalizerToMinus10_10;
+    psiNormalizer = normalizers.entryExpoNormalizerFromMinus4_4;
+    sNormalizer = normalizers.expoNormalizerFromMinus10_10;
+    denormalizer = denormalizers.expoDenormalizerToMinus10_10;
 
     [W, trainingMeanErrors] = backpropagation(psiTrain, sTrain, n, error, iterations, hiddenLayerSizes, g, gDeriv, psiNormalizer, sNormalizer);
 
-    save('./nets/net_6_4_4_4_hyp_0_1.mat','W','psi','s','trainingMeanErrors','g','gDeriv','psiNormalizer','sNormalizer','denormalizer');
+    save('./nets/net_6_4_4_4_expo.mat','W','psi','s','trainingMeanErrors','g','gDeriv','psiNormalizer','sNormalizer','denormalizer');
 
 
