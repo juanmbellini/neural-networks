24d221067b00029e32b6afbc939f68007516f132
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-05-13 22:37:49 -0300

    Add automation scripts for training and testing
    - These function automates
    	- Training of networks by receiving a cell array with paths to training configuration files
    	- Testing of networks by receiving a cell array with paths to neural network files
    - Testing automation function will save plots and csv files with testing results

--- multilayerPerceptron/src/testAndPlot.m
+++ multilayerPerceptron/src/testAndPlot.m
@@ -1,8 +1,9 @@
 % Tests the given neural network and plots graphs
 % @param network_name The name of the network to be tested (must be under nets directory)
-function [] = testAndPlot(network_name)
+function [quadraticMeanError] = testAndPlot(network_name)
 
-    load(strcat('./nets/',network_name)); % Loads netowrk variables into wokspace
+    % load(strcat('./nets/',network_name)); % Loads netowrk variables into wokspace
+    load(network_name); % Loads netowrk variables into wokspace
     
     total = length(s);
     testingSize = length(sTest);
@@ -14,7 +15,8 @@ function [] = testAndPlot(network_name)
 
     % Plotting
     [testedSize, total, sTested, meanError, quadraticMeanError] = calculatePlottingVariables(psi, s, trainingMeanErrors, testingMeanErrors, testedValues, allTestedValues);
-    plotResults(testedValues, sTested, psi, allTestedValues, s, trainingMeanErrors, testingMeanErrors);
+    plotResults(testedValues, sTested, psi, allTestedValues, s, trainingMeanErrors, testingMeanErrors)
 
+    disp(strcat('Quadratic Mean Error is: ', num2str(quadraticMeanError)));
 end
 
